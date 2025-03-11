51b4f46b11af6b784377c187e9a88c81956a1c89
caronf <florimond.caron@etu.umontpellier.fr>
2025-03-11 10:23:17 +0100

    touch invocations/test.sh

--- test/invocation.sh
+++ test/invocation.sh
@@ -12,35 +12,6 @@ curl -s -X POST 'http://localhost:8081/api/users/login' \
         "password": "five"
 }'
 
-# verifier 
 
-curl -X 'POST' \
-  'http://localhost:8084/api/invocations' \
-  -H 'accept: */*' \
-  -H 'Authorization: MftpWGsRGC7A7tOTrEJO9JEGEoFsagMbwhMVPCFgrtPDDTuUoL1W6j5839Aozk5y' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "taux": 0,
-  "monstreDto": {
-    "nom": "string",
-    "niveau": 0,
-    "experiences": 0,
-    "typeElementaire": "FEU",
-    "stats": {
-      "pv": 0,
-      "atq": 0,
-      "def": 0,
-      "vit": 0
-    },
-    "competences": [
-      {
-        "nom": "string",
-        "degatsBase": 0,
-        "ratioDegats": 0,
-        "cooldown": 0,
-        "niveau": 0,
-        "niveauMax": 0
-      }
-    ]
-  }
-}'
+# verifier
+
