e44a0fcf30828d5c4a04ae015336ec5b814f980f
caronf <florimond.caron@etu.umontpellier.fr>
2025-03-12 10:59:41 +0100

    change test/invocations.sh

--- test/invocation.sh
+++ test/invocation.sh
@@ -1,14 +1,14 @@
 curl -s -X POST 'http://localhost:8081/api/users' \
         -H 'Content-Type: application/json' \
         -d '{
-        "username": "jackson",
+        "username": "yoman",
         "password": "five"
 }'
 
 curl -s -X POST 'http://localhost:8081/api/users/login' \
         -H 'Content-Type: application/json' \
         -d '{
-        "username": "jackson",
+        "username": "yoman",
         "password": "five"
 }'
 
