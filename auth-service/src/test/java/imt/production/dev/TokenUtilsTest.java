b3ad3a91d2e81cc9c02371339414c3b6f7d8fa7b
Paul Beaurepaire <119850817+saucebarbeuc@users.noreply.github.com>
2025-03-14 13:15:21 +0100

    test tokenExpiredAfterOneHour

---
+++ auth-service/src/test/java/imt/production/dev/TokenUtilsTest.java
@@ -0,0 +1,20 @@
+package imt.production.dev;
+
+import imt.production.dev.Utils.TokenUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class TokenUtilsTest {
+
+    @Test
+    void tokenExpiredAfterOneHour() {
+        LocalDateTime pastDateTime = LocalDateTime.now().minusHours(1);
+        String token = TokenUtils.generateToken("testUser", pastDateTime);
+        assertTrue(TokenUtils.isTokenExpired(pastDateTime));
+    }
+}
